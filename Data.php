61f7be46454c9a8ba258a1998e1ef4eb848a6a54
Mcdon-ssms <67823655+Mcdon-ssms@users.noreply.github.com>
2020-09-06 22:44:23 +0530

    Add files via upload

---
+++ Data.php
@@ -0,0 +1,51 @@
+<?php
+
+class ItemData
+{
+        private $itemList;
+
+    function __construct(array $itemList)
+    {
+        if (sizeof($itemList)>0)
+        {
+            $this->itemList = $itemList;
+        }
+        else
+        {
+            throw new Exception("No Item Order Record Available");
+        }
+    }
+
+    public function getItemName()
+    {
+        $itemNameList = [];
+
+        foreach($this->itemList as $item)
+        {
+            $itemNameList[] = array("id"=>$item['id'],"name"=>$item['name']);
+        }
+
+        return json_encode($itemNameList);
+    }
+
+    public function getItemById($id)
+    {
+        $response = ["In-Valid Order ID"];
+        if($id)
+        {
+            foreach($this->itemList as $item) 
+            {
+                if ($id == $item['id'])
+                {
+                    $response = $item;
+                    break;
+                }
+            }
+        }
+        return json_encode($response);
+    }
+
+
+
+}
+?>
